b1c725fac73aa322fefc71f70e7491bbfc62d654
7byte1337 <40406824+7byte1337@users.noreply.github.com>
2018-06-19 23:49:15 +0300

    Add files via upload

---
+++ 1337.sh
@@ -0,0 +1,12 @@
+# info
+cd
+figlet Meniu VPS 1337 V.I.P
+figlet Va pupa 7Byte <3 
+figlet Este Gata
+figlet Configurarea 
+sleep 9
+
+#Instalam iftop
+cd
+apt-get install iftop
+iftop
---
+++ Despre.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear 
+
+echo "#===============================================#"
+echo "#     Scriptul este valabil pentru OS-urile     #"
+echo "#-----------------------------------------------#"
+echo "#    Ubuntu 14,16 ,DEBIAN 7,8,9, 32bit & 64bit  #"
+echo "#                     VPS                       #"
+echo "#=====================PERU======================#"
+echo "● Date pentru instalare" 
+echo "☑️ OpenSSH, port: 22" 
+echo "☑️ Dropbear, port: 443" 
+echo "☑️ Squid3 Port: 8080, 3128"
+echo "☑️ Proxy Python: 1080,53"
+echo "☑️ Badvpn: Port:7300" 
+echo "☑️ Webmin: http://IPLaVPS:10000/" 
+echo "☑️ etc"
+echo "Sa te intorci la loc la meniu foloseste : [menu]" 
+echo "" 
